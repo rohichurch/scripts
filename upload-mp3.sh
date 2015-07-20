b86244e600cf3ab00a928285094218de6cc776d8
Alex Ortiz-Rosado <alex@rohichurch.org>
2015-07-19 22:57:44 -0700

    Send email notification when upload has completed

--- upload-mp3.sh
+++ upload-mp3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # This script is used for preparing the weekly MP3 sermon.
-# Run this script after the sermon has been editted and 
-# exported to the $DEFAULT_SONGS_DIR. 
+# Run this script after the sermon has been editted and
+# exported to the $DEFAULT_SONGS_DIR.
 #
 # The script will take care of:
 #   1) Converting the WAV file to MP3
@@ -12,10 +12,8 @@
 # Once completed, a link to the MP3 file on Amazon AWS will
 # be provided which will then be used for adding the sermon
 # to the church's website.
-#
-# You must first install and setup the AWS Command Line Interface
-# Tools available at:
-# http://docs.aws.amazon.com/AWSEC2/latest/CommandLineReference/set-up-ec2-cli-linux.html
+
+NOTIFICATION_ADDRESS="devops-team@rohichurch.org"
 
 CURRENT_YEAR=$(date +"%Y")
 DEFAULT_SONGS_DIR="$HOME/Desktop/Mixdowns"
@@ -32,16 +30,16 @@ function banner {
 function get_sermon_info {
   read -p "Enter the speaker's name: " SPEAKER_NAME
   read -p "Enter the sermon title: " SERMON_TITLE
-  SERMON_MP3_FILENAME="$SPEAKER_NAME -"
+  SERMON_MP3_FILENAME="${SPEAKER_NAME} -"
 
   read -p "Is this sermon part of a series? [y/n]: " IS_SERIES
   if [[ $IS_SERIES =~ ^y ]] ; then
     read -p "Enter sermon series name: " SERMON_SERIES
-    SERMON_MP3_FILENAME="$SERMON_MP3_FILENAME $SERMON_SERIES -" 
+    SERMON_MP3_FILENAME="${SERMON_MP3_FILENAME} ${SERMON_SERIES} -"
     read -p "What number is this in the series?: " TRACK_NUMBER
-    SERMON_MP3_FILENAME="$SERMON_MP3_FILENAME $TRACK_NUMBER -"
+    SERMON_MP3_FILENAME="${SERMON_MP3_FILENAME} ${TRACK_NUMBER} -"
   fi
-  SERMON_MP3_FILENAME="$SERMON_MP3_FILENAME $SERMON_TITLE.mp3"
+  SERMON_MP3_FILENAME="${SERMON_MP3_FILENAME} ${SERMON_TITLE}.mp3"
 }
 
 function convert_to_mp3 {
@@ -57,7 +55,7 @@ function convert_to_mp3 {
   	  --tg "Sermons" \
   	  --ti "${DROPBOX_SERMON_DIR}/album-cover_current-series.jpg" \
           --add-id3v2 \
-  	"${1}" "$DROPBOX_SERMON_DIR/${2}" 
+  	"${1}" "$DROPBOX_SERMON_DIR/${2}"
   else
   	lame \
   	  -b 64 \
@@ -69,13 +67,13 @@ function convert_to_mp3 {
   	  --tg "Sermons" \
   	  --ti "${DROPBOX_SERMON_DIR}/album-cover_rohi-logo.jpg" \
   	  --add-id3v2 \
-  	"${1}" "$DROPBOX_SERMON_DIR/${2}" 
+  	"${1}" "$DROPBOX_SERMON_DIR/${2}"
   fi
 
 }
 
 function upload_to_s3 {
-  aws s3 cp "$1" $AWS_S3_PODCAST_BUCKET --grants \
+  aws s3 cp "${1}" ${AWS_S3_PODCAST_BUCKET} --grants \
     read=uri=http://acs.amazonaws.com/groups/global/AllUsers \
     full=emailaddress=alex@rohichurch.org
 }
@@ -85,19 +83,41 @@ function get_podcast_aws_url {
   echo "http://rohichurch.s3.amazonaws.com/${AWS_S3_PODCASTS_DIR}${aws_sermon_filename}"
 }
 
-# function main
+function send_email_notification {
+  local notification_address=$1
+  local sermon_title=$2
+  local speaker_name=$3
+  local mp3_location=$4
+  mail -s "Podcast Uploaded: ${sermon_title}" ${notification_address} <<EOF
+  A new podcast has been uploaded.
+
+   * Title: ${sermon_title}
+   * Speaker: ${speaker_name}
+   * MP3 Location: ${mp3_location}
+
+  To add the message to the Rohi Christian Church podcast, open
+  http://www.rohichurch.org/admin and add go to Sermons > Add New
+EOF
+}
+
 get_sermon_info
 
 banner "Converting AIFF file to MP3..."
-banner "$LAST_WAV_FILE_ADDED"
-convert_to_mp3 "$LAST_WAV_FILE_ADDED" "$SERMON_MP3_FILENAME"
+banner "${LAST_WAV_FILE_ADDED}"
+convert_to_mp3 "${LAST_WAV_FILE_ADDED}" "${SERMON_MP3_FILENAME}"
 banner "Done converting."
 
 banner "Uploading to Amazon AWS..."
-upload_to_s3 "$DROPBOX_SERMON_DIR/$SERMON_MP3_FILENAME"
+upload_to_s3 "${DROPBOX_SERMON_DIR}/${SERMON_MP3_FILENAME}"
 banner "Done uploading."
 
 banner "Open http://www.rohichurch.org/admin and add go to Sermons > Add New"
-get_podcast_aws_url
+MP3_LOCATION=$(get_podcast_aws_url)
+
+send_email_notification \
+  "${NOTIFICATION_ADDRESS}" \
+  "${SERMON_TITLE}" \
+  "${SPEAKER_NAME}" \
+  "${MP3_LOCATION}"
 
 exit 0
